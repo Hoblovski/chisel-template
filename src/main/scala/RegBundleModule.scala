b6d9757dfe9e58ab637c5960b0e8e83d9b8a63fb
dzy <daizy15@mails.tsinghua.edu.cn>
2018-07-24 19:42:21 +0800

    add reg init example

--- src/main/scala/RegBundleModule.scala
+++ src/main/scala/RegBundleModule.scala
@@ -8,13 +8,19 @@ class MyBundle extends Bundle {
   val v2 = UInt(32.W)
 }
 
+object MyBundle {
+  val init = Wire(new MyBundle)
+  init.v1 := "h_1111_1111".U
+  init.v2 := "h_2222_2222".U
+}
+
 class RegBundleModule extends Module {
   val io = IO(new Bundle {
     val dataIn = Input(new MyBundle)
     val data = Output(new MyBundle)
   })
 
-  val _data = Reg(new MyBundle())
+  val _data = RegInit(MyBundle.init)
   _data := io.dataIn
   io.data := _data
 }
